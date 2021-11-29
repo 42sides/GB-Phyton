7ceb9e0317322959277b6409749d3c264c659dd6
42sides <61024064+42sides@users.noreply.github.com>
2021-11-29 08:53:51 +0300

    Update Lesson_4.5.py

--- Lesson_4.5.py
+++ Lesson_4.5.py
@@ -2,7 +2,7 @@ from functools import reduce
 
 
 def function(first_element, second_element):
-    return first_element + second_element
+    return first_element * second_element
 
 
 my_set = [el for el in range(100, 1001) if el % 2 == 0]
